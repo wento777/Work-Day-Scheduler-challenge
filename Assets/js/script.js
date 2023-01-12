4bfc52f209f3a9b62ddb1d7bc74b4dbe18d31b02
wento <florea_andreiwento@yahoo.com>
2023-01-12 00:15:02 -0600

    deleted past,future and present class from implemented code

---
+++ Assets/js/script.js
@@ -0,0 +1,66 @@
+// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
+// the code isn't run until the browser has finished rendering all the elements
+// in the html.
+$(document).ready(function () {
+  $(".saveBtn").on("click", function(){
+    var time = $(this).parent().attr('id');
+    var notes = $(this).siblings(".description").val();
+    localStorage.setItem(time, notes);
+
+
+  })
+
+  // TODO: Add a listener for click events on the save button. This code should
+  // use the id in the containing time-block as a key to save the user input in
+  // local storage. HINT: What does `this` reference in the click listener
+  // function? How can DOM traversal be used to get the "hour-x" id of the
+  // time-block containing the button that was clicked? How might the id be
+  // useful when saving the description in local storage?
+
+// adde a function timer
+function timer(){
+  //dayjs().hour()  gets current hour//
+ var currenthour = dayjs().hour();
+  $(".time-block").each(function(){
+    var time = parseInt( $(this).attr('id').split('-')[1]);
+    if (time < currenthour){
+      
+      $(this).addClass('past');
+    } else if (time === currenthour){
+      $(this).addClass('present');
+    } else{
+      $(this).addClass('future');
+    }
+
+  })
+}
+timer()
+
+//
+  // TODO: Add code to apply the past, present, or future class to each time
+  // block by comparing the id to the current hour. HINTS: How can the id
+  // attribute of each time-block be used to conditionally add or remove the
+  // past, present, and future classes? How can Day.js be used to get the
+  // current hour in 24-hour time?
+  //
+  // TODO: Add code to get any user input that was saved in localStorage and set
+  // the values of the corresponding textarea elements. HINT: How can the id
+  // attribute of each time-block be used to do this?
+  //
+  
+//to target the text area, set the value to whathever you can find in the key// reapet till 17 hour
+
+$("#hour-9 .description").val(localStorage.getItem("hour-9"));
+$("#hour-10 .description").val(localStorage.getItem("hour-10"));
+$("#hour-11 .description").val(localStorage.getItem("hour-11"));
+$("#hour-12 .description").val(localStorage.getItem("hour-12"));
+$("#hour-13 .description").val(localStorage.getItem("hour-13"));
+$("#hour-14 .description").val(localStorage.getItem("hour-14"));
+$("#hour-15 .description").val(localStorage.getItem("hour-15"));
+$("#hour-16 .description").val(localStorage.getItem("hour-16"));
+$("#hour-17 .description").val(localStorage.getItem("hour-17"));
+  
+
+
+  // TODO: Add code to display the current date in the header of the page.
+});
